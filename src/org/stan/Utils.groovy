931fcf1b8e317573b77a440a6494c091e8b5b145
Sean Talts <sean.talts@gmail.com>
2017-11-01 15:35:44 -0400

    Try alternative version

--- src/org/stan/Utils.groovy
+++ src/org/stan/Utils.groovy
@@ -4,7 +4,22 @@ import hudson.model.Result
 import jenkins.model.CauseOfInterruption.UserInterruption
 
 def killOldBuilds() {
-  while(currentBuild.rawBuild.getPreviousBuildInProgress() != null) {
-    currentBuild.rawBuild.getPreviousBuildInProgress().doKill()
+  def hi = Hudson.instance
+  def pname = env.JOB_NAME.split('/')[0]
+
+  hi.getItem(pname).getItem(env.JOB_BASE_NAME).getBuilds().each{ build ->
+    def exec = build.getExecutor()
+
+    if (build.number != currentBuild.number && exec != null) {
+      exec.interrupt(
+        Result.ABORTED,
+        new CauseOfInterruption.UserInterruption(
+          "Aborted by #${currentBuild.number}"
+        )
+      )
+      println("Aborted previous running build #${build.number}")
+    } else {
+      println("Build is not running or is current build, not aborting - #${build.number}")
+    }
   }
 }
