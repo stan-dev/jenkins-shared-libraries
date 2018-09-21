d1d5da5877e4882de765b9e4790926ed699f8fd3
Sean Talts <sean.talts@gmail.com>
2018-09-21 11:31:10 -0400

    Allow either OSX or Linux again

--- src/org/stan/Utils.groovy
+++ src/org/stan/Utils.groovy
@@ -26,7 +26,7 @@ def isBranch(env, String b) { env.BRANCH_NAME == b }
 
 def updateUpstream(env, String upstreamRepo) {
     if (isBranch(env, 'develop')) {
-        node('linux') {
+        node('osx || linux') {
             retry(3) {
                 deleteDir()
                 withCredentials([
