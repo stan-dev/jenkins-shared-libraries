67bc2b08599b4684062ee2b85f8933cd1a94a10d
unknown <serban.nicusor@toptal.com>
2020-03-12 20:16:41 +0100

    Removed debug code

--- src/org/stan/Utils.groovy
+++ src/org/stan/Utils.groovy
@@ -55,12 +55,7 @@ def isBuildAReplay() {
 
 def verifyChanges(String sourceCodePaths) {
 
-    println sh(script: "pwd", returnStdout: true)
-    println sh(script: "ls -lhart", returnStdout: true)
-    println sh(script: "git status", returnStdout: true)
-    println sh(script: "git rev-parse HEAD", returnStdout: true)
-
-    def commitHash = sh(script: "git rev-parse HEAD", returnStdout: true).trim()
+    def commitHash = sh(script: "git rev-parse HEAD | tr '\\n' ' '", returnStdout: true)
     def changeTarget = ""
 
     if (env.CHANGE_TARGET) {
