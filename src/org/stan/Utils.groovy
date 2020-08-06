388e4086cb7b16890035b70ea88d35346ba1ea82
Serban Nicusor <nicusor.serban@sre.work>
2020-08-06 02:28:51 +0200

    Added --replace-all flag

--- src/org/stan/Utils.groovy
+++ src/org/stan/Utils.groovy
@@ -58,7 +58,7 @@ def verifyChanges(String sourceCodePaths) {
     def commitHash = sh(script: "git rev-parse HEAD | tr '\\n' ' '", returnStdout: true)
     def changeTarget = ""
 
-    sh(script: "git config remote.origin.fetch '+refs/heads/*:refs/remotes/origin/*'", returnStdout: true)
+    sh(script: "git config remote.origin.fetch '+refs/heads/*:refs/remotes/origin/*' --replace-all", returnStdout: true)
     sh(script: "git fetch --all", returnStdout: true)
   
     if (env.CHANGE_TARGET) {
