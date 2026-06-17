fd1c9c843f504837ea4edffd5a670ad86f784b63
Dylan Simon <dylan@dylex.net>
2026-06-17 09:18:24 -0400

    filesChanged: rewrite verifyChanges

---
+++ vars/filesChanged.groovy
@@ -0,0 +1,50 @@
+/* return true if any of the listed files are changed
+ * - either by the current PR
+ * - or since the last build
+ * these can be overridden by "[ci skip]" (always false) or "[ci run all]" (always true) in the last commit message
+ *
+ * This replaces utils.verifyChanges, but returns its negation
+ */
+
+def call(String[] paths) {
+  def commitMsg = ""
+  if (currentBuild.changeSets && currentBuild.changeSets[0]) {
+    commitMsg = currentBuild.changeSets[0][0].getMsg()
+  }
+
+  // If last commit message contains [ci skip] the current build will be skipped
+  if (commitMsg.contains("[ci skip]")) {
+    return false
+  }
+
+  // If last commit message contains [ci run all] we will run all stages no matter of source code changes
+  if (commitMsg.contains("[ci run all]")) {
+    return true
+  }
+
+  def changeTarget = null
+  if (env.CHANGE_TARGET) {
+    changeTarget = "refs/remotes/origin/${env.CHANGE_TARGET}"
+  } else {
+    /* always run builds on downstream_ branches?? */
+    if (env.BRANCH_NAME == "downstream_tests" || env.BRANCH_NAME == "downstream_hotfix") {
+      return true
+    }
+  }
+
+  if (changeTarget) {
+    /* FIXME: unsafe path shell handling */
+    def pathstr = paths.join(" ")
+    def diff = sh(script: "git diff --quiet ${changeTarget} -- ${pathstr}", returnStatus: true)
+    return (diff != 0)
+  }
+
+  return currentBuild.changeSets.any { changeSet ->
+    changeSet.any { change ->
+      change.getAffectedPaths().any { path ->
+        /* FIXME: overinclusive prefix match */
+        path.startsWithAny(paths)
+      }
+    }
+  }
+}
